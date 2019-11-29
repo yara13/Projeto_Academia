8a03bf79084bc5d8f7b900050a558a95751873ca
Yara <ybn242@gmail.com>
2019-11-29 15:13:22 -0300

    validando o campo login
    fazendo a validação do campo login

--- index.php
+++ index.php
@@ -23,31 +23,41 @@
 
   <div class="container">
     <div class="card card-login mx-auto mt-5">
-      <div class="card-header">Login</div>
+      <div class="card-header">Login CPF/E-mail:</div>
       <div class="card-body">
-        <form class="was-validated">
-          <div class="form-group">
-            <div class="form-label-group">
-              <input type="number" id="inputEmail" class="form-control" placeholder="Email" required="required" autofocus="autofocus">
-              <label for="inputEmail">CPF</label>
-            </div>
+       <form class="needs-validation" novalidate action="admin.php" method="POST">
+
+        <div class="form-group">
+          <input type="text" class="form-control" id="login" placeholder="Digite seu CPF ou e-mail" required>
+          <div class="invalid-feedback">
+            Login Invalido
           </div>
-          <div class="form-group">
-            <div class="form-label-group">
-              <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required="required">
-              <label for="inputPassword">Senha</label>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" value="remember-me">
-                Lembrar Senha
-              </label>
-            </div>
-          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="form-label-group">
+            <input type="password" id="senha" class="form-control" placeholder="Digite sua Senha" required="required">
+            <label for="inputPassword">Digite sua senha</label>
+            <div class="invalid-feedback">
+             Senha Invalida
+           </div>
+         </div>
+       </div>
+
+       <div class="form-group">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me">
+            Lembrar Senha
+          </label>
+        </div>
+      </div>
+
+      <button class="btn btn-primary btn-sm" type="submit">Logar Aluno</button>
+      <button class="btn btn-primary btn-sm" type="submit">Logar Administrador</button>
+<!--
           <a class="btn btn-primary btn-block" href="areaaluno.php">Logar Aluno</a>
-           <a class="btn btn-primary btn-block" href="admin.php">Logar Administrador</a>
+          <a class="btn btn-primary btn-block" href="admin.php">Logar Administrador</a>-->
         </form>
         <div class="text-center">
           <a class="d-block small" href="forgot-password.php">Esqueceu sua Senha?</a>
@@ -64,5 +74,25 @@
   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
 </body>
+<script type="text/javascript">
 
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.getElementsByClassName('needs-validation');
+// Loop over them and prevent submission
+var validation = Array.prototype.filter.call(forms, function(form) {
+  form.addEventListener('submit', function(event) {
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+  }, false);
+});
+}, false);
+  })();
+
+</script>
 </html>
